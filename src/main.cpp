10765a617f574e325d8ffc155352f3fdf7d55d14
A55adon <simonwa08@gmx.de>
2025-09-26 18:47:39 +0200

    Got rmlui example working

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,59 @@
-#include <iostream>
 #include <RmlUi/Core.h>
+#include <RmlUi/Debugger.h>
+#include <RmlUi_Backend.h>
+#include <Shell.h>
 
+#include <RmlUi_Include_Windows.h>
+int APIENTRY WinMain(HINSTANCE /*instance_handle*/, HINSTANCE /*previous_instance_handle*/, char* /*command_line*/, int /*command_show*/)
+{
+	int window_width = 1024;
+	int window_height = 768;
 
-int main() {
+	if (!Shell::Initialize())
+		return -1;
 
-    std::cout << "Hello World!\n";
+	if (!Backend::Initialize("TCast", window_width, window_height, true))
+	{
+		Shell::Shutdown();
+		return -1;
+	}
 
-    return 0;
+	Rml::SetSystemInterface(Backend::GetSystemInterface());
+	Rml::SetRenderInterface(Backend::GetRenderInterface());
+
+	Rml::Initialise();
+
+	Rml::Context* context = Rml::CreateContext("main", Rml::Vector2i(window_width, window_height));
+	if (!context)
+	{
+		Rml::Shutdown();
+		Backend::Shutdown();
+		Shell::Shutdown();
+		return -1;
+	}
+
+	Rml::Debugger::Initialise(context);
+	Shell::LoadFonts();
+
+	// Load and show the tutorial document.
+	if (Rml::ElementDocument* document = context->LoadDocument("C:/Code/TCast/src/tutorial.rml"))
+		document->Show();
+
+	bool running = true;
+	while (running)
+	{
+		running = Backend::ProcessEvents(context, &Shell::ProcessKeyDownShortcuts, true);
+
+		context->Update();
+
+		Backend::BeginFrame();
+		context->Render();
+		Backend::PresentFrame();
+	}
+
+	Rml::Shutdown();
+	Backend::Shutdown();
+	Shell::Shutdown();
+
+	return 0;
 }

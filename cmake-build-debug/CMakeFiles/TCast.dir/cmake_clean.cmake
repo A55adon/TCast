da9a2dbf8e4b6a592cb0d5c9d0de5e9d0a298967
A55adon <simonwa08@gmx.de>
2025-10-18 23:34:41 +0200

    fixed cmakelists to include the default dlls

---
+++ cmake-build-debug/CMakeFiles/TCast.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCast.dir/libs/RmlUi/Backends/RmlUi_Backend_GLFW_GL3.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Backends/RmlUi_Backend_GLFW_GL3.cpp.obj.d"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Backends/RmlUi_Platform_GLFW.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Backends/RmlUi_Platform_GLFW.cpp.obj.d"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Backends/RmlUi_Renderer_GL3.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Backends/RmlUi_Renderer_GL3.cpp.obj.d"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/PlatformExtensions.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/PlatformExtensions.cpp.obj.d"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/RendererExtensions.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/RendererExtensions.cpp.obj.d"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/Shell.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/Shell.cpp.obj.d"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/ShellFileInterface.cpp.obj"
+  "CMakeFiles/TCast.dir/libs/RmlUi/Samples/shell/src/ShellFileInterface.cpp.obj.d"
+  "CMakeFiles/TCast.dir/src/Projector.cpp.obj"
+  "CMakeFiles/TCast.dir/src/Projector.cpp.obj.d"
+  "CMakeFiles/TCast.dir/src/Window.cpp.obj"
+  "CMakeFiles/TCast.dir/src/Window.cpp.obj.d"
+  "CMakeFiles/TCast.dir/src/main.cpp.obj"
+  "CMakeFiles/TCast.dir/src/main.cpp.obj.d"
+  "TCast.exe"
+  "TCast.exe.manifest"
+  "TCast.pdb"
+  "libTCast.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
